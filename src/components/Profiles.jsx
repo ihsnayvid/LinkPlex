5e9e1af720b9aeef1f69318553af9025d9e91969
RahulPoddar1602 <rahul1999p@gmail.com>
2023-07-01 14:14:20 +0530

    designed profiles page

--- src/components/Profiles.jsx
+++ src/components/Profiles.jsx
@@ -3,13 +3,17 @@ import { useEffect, useState } from 'react';
 import { Box, Card, CardContent, CardMedia, Button, Typography, styled, TextField } from '@mui/material';
 import { db } from '../config/firebase';
 import { getDocs, collection } from 'firebase/firestore';
+// import { filter } from 'lodash';
 
 
 const ProfileCard = styled(Card)`
+  background:#FCFFE7;
+  ${'' /* border:4px solid rgb(39,39,39); */}
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  box-shadow: 0.7rem 0.7rem rgb(26,26,26);
   width: 200px;
   height: 200px;
   margin: 16px;
@@ -18,32 +22,35 @@ const ProfileCard = styled(Card)`
 const ArtistImage = styled(CardMedia)`
   width: 100px;
   height: 100px;
+  ${'' /* border:4px solid rgb(39,39,39); */}
   border-radius: 50%;
   margin-top: 16px;
 `;
-
+const NameButton = styled(Button)`
+  background:#3CCF4E;
+  width:100%;
+`
 const SearchBox = styled(TextField)`
-    width: 50%;
+  width: 50%;
   position: fixed;
-  top: 10px;
+  top: 10%;
   left: 50%;
   transform: translateX(-50%);
   z-index: 999;
   background-color: white;
 `;
-
 // const Profiles = ({ musicArtists }) => {
   const Profiles = () => {
   const [allUsers, setAllUsers] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
 
-  // const handleSearchChange = (event) => {
-  //   setSearchQuery(event.target.value);
-  // };
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
 
-  // const filteredArtists = musicArtists.filter((artist) =>
-  //   artist.name.toLowerCase().includes(searchQuery.toLowerCase())
-  // );
+  const filteredArtists = allUsers.filter((artist) =>
+    artist.Name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   useEffect(() =>{
     const getAllUsers = async () => {
@@ -67,24 +74,24 @@ const SearchBox = styled(TextField)`
   }, []);
   return (
     <Box>
-      {/* <SearchBox
+      <SearchBox
         label="Search by User Name"
         value={searchQuery}
         onChange={handleSearchChange}
         variant="outlined"
       />
-      <Box display="flex" flexWrap="wrap" justifyContent="center" marginTop={8}>
+      <Box display="flex" flexWrap="wrap" justifyContent="center" marginTop={20}>
         {filteredArtists.map((artist, index) => (
           <ProfileCard key={index}>
-            <ArtistImage image={artist.imageUrl} alt={artist.name} />
-            <CardContent>
-              <Button variant="contained" color="primary">
-                {artist.name}
-              </Button>
+            <ArtistImage image={artist.Profile} alt={artist.name} />
+            <CardContent >
+              <NameButton  variant="contained" color="primary">
+                {artist.Name}
+              </NameButton>
             </CardContent>
           </ProfileCard>
         ))}
-      </Box> */}
+      </Box>
     </Box>
   );
 };
